4f1f4856ba73f660ff88e2d8a4bd86f4133acd95
Odelya yakowich <odelyaha1998@gmail.com>
2023-04-24 19:23:47 +0300

    add configure of logs and parameters

--- chess_gui.py
+++ chess_gui.py
@@ -10,6 +10,7 @@ import pygame as py
 
 import ai_engine
 from enums import Player
+import logging
 
 """Variables"""
 WIDTH = HEIGHT = 512  # width and height of the chess board
@@ -18,6 +19,21 @@ SQ_SIZE = HEIGHT // DIMENSION  # the size of each of the squares in the board
 MAX_FPS = 15  # FPS for animations
 IMAGES = {}  # images for the chess pieces
 colors = [py.Color("white"), py.Color("gray")]
+
+# Create a logger object
+logger = logging.getLogger('my_logger')
+logger.setLevel(logging.DEBUG)
+
+# Create a file handler
+file_handler = logging.FileHandler('my_logs.log', mode='w')
+file_handler.setLevel(logging.DEBUG)
+
+# Create a formatter and add it to the file handler
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+
+# Add the file handler to the logger
+logger.addHandler(file_handler)
 
 # TODO: AI black has been worked on. Mirror progress for other two modes
 def load_images():
@@ -119,11 +135,17 @@ def main():
     valid_moves = []
     game_over = False
 
+    knight_steps = 0
+    _is_check = 0
+    valid = False
+
     ai = ai_engine.chess_ai()
     game_state = chess_engine.game_state()
     if human_player is 'b':
         ai_move = ai.minimax_black(game_state, 3, -100000, 100000, True, Player.PLAYER_1)
         game_state.move_piece(ai_move[0], ai_move[1], True)
+
+
 
     while running:
         for e in py.event.get():
